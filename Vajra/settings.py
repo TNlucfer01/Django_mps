a04a46341f113d4e1bdf2a65692b5b2e66a6d003
TNlucfer01 <ihdaa2006@gmail.com>
2026-04-08 21:45:43 +0530

    After supase

--- Vajra/settings.py
+++ Vajra/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,14 +74,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "Vajra.wsgi.application"
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
